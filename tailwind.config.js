8b1b28aabcab7e8bb5a3996a301cdf62e497abea
Bhavy <bhavymehta10@gmail.com>
2023-08-16 14:37:35 +0530

    product page added and updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/web-pg/index.html"],
+  content: ["./src/**/*.{html,js}", "./src/web-pg/index.html", "./src/pr-pg/index.html"],
   theme: {
     extend: {},
   },
